3d9f4b7576bbc02618910543693b91f92c85ebb1
m0zaid <mdzaid2798@gmail.com>
2025-07-22 14:41:50 +0530

    Starting my new project NewsNest App

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import NavBar from './components/NavBar'
+import News from './components/News'
+import NewsItem from './components/NewsItem'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <News />
+
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+      </div>
+    )
+  }
+}
